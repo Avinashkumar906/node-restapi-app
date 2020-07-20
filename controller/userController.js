772d0d7b1c4fe1c442ea208068274d55a4a41b56
Avinash <Avinash@tracfone.com>
2020-07-20 18:28:22 +0530

    validation for ngx5profile data

--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,11 @@
 const User = require('../model/user')
-const bcrypt = require('bcryptjs')
 const log = require('log-to-file')
 
 exports.getUserData = async (req, res, next) => {
-    try {
-        
-        var mail = process.env.DEFAULT_EMAIL
-        var user = await User.findOne({ email: mail})
+    try {        
+        log(`userController.getUserData() <br/>`);
+        var mail = process.env.DEFAULT_EMAIL;
+        var user = await User.findOne({ email: mail});
         res.status(201).json(user)
     } catch (error) {
         res.status(400).json(error)
@@ -15,14 +14,14 @@ exports.getUserData = async (req, res, next) => {
 
 exports.postNameAndBio = async (req,res)=>{
     try {
-        // req.user = {email:'avinashkumar906@gmail.com'}
-        if(req.user){
-            let mail = req.user.email 
-            let user = await User.findOne({email:mail})
+        log(`userController.postNameAndBio() user logged: ${JSON.stringify(req.user)}<br/>`);
+        if(req.user && req.user.email == process.env.DEFAULT_EMAIL){
+            let mail = process.env.DEFAULT_EMAIL;
+            let user = await User.findOne({email:mail});
                 user.name = req.body.name;
                 user.bio = req.body.bio;
-            user.save(()=>{
-                res.status(201).json({message:"updated successfull!"})
+            user.save((err,result)=>{
+                !err ? res.status(201).json(result) : res.status(401).json({message:"User data not found!",err})
             })
         } else {
             res.status(401).json( { message:"Unauthorised User!" } )
@@ -34,13 +33,17 @@ exports.postNameAndBio = async (req,res)=>{
 
 exports.postUserData = async (req,res,next)=>{
     try {
-        let mail = req.user ? req.user.email : res.status(401).json({message:"Unauthorised User!"});
-        let user = await User.findOne({email:mail})
+        log(`userController.postUserData() user logged: ${JSON.stringify(req.user)}<br/>`)
+        if(req.user && req.user.email == process.env.DEFAULT_EMAIL){
+            let mail = process.env.DEFAULT_EMAIL;
+            let user = await User.findOne({email:mail});
             user.contact = req.body;
-            user.save(()=>{
-                res.status(201).json({message:"updated successfull!"})
+            user.save((err,result)=>{
+                !err ? res.status(201).json(result) : res.status(401).json({message:"User data not found!",err})
             })
-
+        } else {
+            res.status(401).json( { message:"Unauthorised User!" } )
+        }
     } catch (error) {
         res.status(400).json({message:"some error occured!"})
     }
@@ -48,36 +51,51 @@ exports.postUserData = async (req,res,next)=>{
 
 exports.postAboutSection = async (req,res,next)=>{
     try {
-        let mail = req.user ? req.user.email : res.status(401).json({message:"Unauthorised User!"});
-        let user = await User.findOne({email:mail})
+        log(`userController.postAboutSection() user logged: ${JSON.stringify(req.user)}<br/>`)
+        if(req.user && req.user.email == process.env.DEFAULT_EMAIL){
+        let mail = process.env.DEFAULT_EMAIL;
+        let user = await User.findOne({email:mail});
             user.about = req.body;
-            user.save(()=>{
-                res.status(201).json({message:"updated successfull!"})
+            user.save((err,result)=>{
+                !err ? res.status(201).json(result) : res.status(401).json({message:"User data not found!",err})
             })
+        } else {
+            res.status(401).json( { message:"Unauthorised User!" } )
+        }
     } catch (error) {
         res.status(400).json({message:"some error occured!"})
     }
 }
 exports.postPortfolioSection = async (req,res,next)=>{
     try {
-        let mail = req.user ? req.user.email : res.status(401).json({message:"Unauthorised User!"});
+        log(`userController.postPortfolioSection() user logged: ${JSON.stringify(req.user)}<br/>`)
+        if(req.user && req.user.email == process.env.DEFAULT_EMAIL){
+        let mail = process.env.DEFAULT_EMAIL;
         let user = await User.findOne({email:mail})
             user.portfolio = req.body;
-            user.save(()=>{
-                res.status(201).json({message:"updated successfull!"})
+            user.save((err,result)=>{
+                !err ? res.status(201).json(result) : res.status(401).json({message:"User data not found!",err})
             })
+        } else {
+            res.status(401).json( { message:"Unauthorised User!" } )
+        }
     } catch (error) {
         res.status(400).json({message:"some error occured!"})
     }
 }
 exports.postResumeSection = async (req,res,next)=>{
     try {
-        let mail = req.user ? req.user.email : res.status(401).json({message:"Unauthorised User!"});
-        let user = await User.findOne({email:mail})
+        log(`userController.postResumeSection() user logged: ${JSON.stringify(req.user)}<br/>`)
+        if(req.user && req.user.email == process.env.DEFAULT_EMAIL){
+            let mail = process.env.DEFAULT_EMAIL;
+            let user = await User.findOne({email:mail})
             user.resume = req.body;
-            user.save(()=>{
-                res.status(201).json({message:"updated successfull!"})
+            user.save((err,result)=>{
+                !err ? res.status(201).json(result) : res.status(401).json({message:"User data not found!",err})
             })
+        } else {
+            res.status(401).json( { message:"Unauthorised User!" } )
+        }
     } catch (error) {
         res.status(400).json({message:"some error occured!"})
     }
